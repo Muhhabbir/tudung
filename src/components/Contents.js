c3c420014aad2308417dcf555a974e94f65b859b
Muhhabbir <101520159+Muhhabbir@users.noreply.github.com>
2022-05-29 16:44:47 +0700

    Update Contents.js

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -10,21 +10,21 @@ const Contents = () => {
             <div class="card">
                 <img src="https://asset.kompas.com/crops/taRfpgjlHmmataHLSuPIEbh6rZI=/101x0:601x500/340x340/data/photo/2021/08/31/612e464ae9750.jpg" class="card-img-bottom" alt="..."></img>
                 <div class="card-body">
-                    <h5 class="card-title">Judul Card</h5>
+                    <h5 class="card-title">Ras Mongeudong</h5>
                     <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor.</p>
                 </div>
             </div>
             <div class="card">
                 <img src="https://asset.kompas.com/crops/WbjMFSsixI5N-mc6KnymBbkqqoY=/192x128:1728x1152/750x500/data/photo/2021/06/21/60d031a793a44.jpg" class="card-img-top" alt="..."></img>
                 <div class="card-body">
-                    <h5 class="card-title">Judul Card</h5>
+                    <h5 class="card-title">Ras Rahayu</h5>
                     <p class="card-text">Vestibulum id metus ac nisl bibendum nibh scelerisque non purus in suspendisse.</p>
                 </div>
             </div>
             <div class="card">
                 <img src="https://kucingpedia.com/wp-content/uploads/2018/05/Gambar-Kucing-Scottish-Fold-Lucu-630x380.jpg" class="card-img-top" alt="..."></img>
                 <div class="card-body">
-                    <h5 class="card-title">Judul Card</h5>
+                    <h5 class="card-title">Ras Kasdroen</h5>
                     <p class="card-text">Pulvinar leo id risus enim. Bibendum varius tincidunt pellentesque malesuada in.</p>
                 </div>
             </div>
