342be13887eb8e3f94dc5e769afc16498ae7d707
Sharhan Anhar <jancoegsekali@gmail.com>
2022-06-04 17:37:52 +0700

    add grid on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,48 @@
-import { Container } from 'react-bootstrap';
-import '../style/landingPage.css';
+import { Container, Row, Col } from "react-bootstrap";
+import "../style/landingPage.css";
 const Footer = () => {
-    return (
-        <footer className='footer fluid bg-dark text-white'>
-         <div>
-            <Container>
-            <h6>Newsletter subscription</h6>
-            <p>Sign up for Tudungpeople updates to receive information about new arrivals and future events.</p>
-            </Container>
-         </div>
-         <div>
-             <Container>
-                <h6>Account</h6>
-                <p>Rewards Programme</p>
-                <p>Returns & Exchanges</p>
-             </Container>
-         </div>
-</footer>
-    )
-}
+  return (
+    <footer className="footer fluid bg-dark text-white">
+      <div>
+        <Container>
+          <Row>
+            <Col xs>
+              Newsletter subscription
+              <p>
+                Sign up for Tudungpeople updates to receive information about
+                new arrivals and future events.
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div>
+        <Container>
+          <Row>
+            <Col xs={{ order: 12 }}>
+              <h6>Account</h6>
+              <p>Rewards Programme</p>
+              <p>Returns & Exchanges</p>
+            </Col>
+            <Col xs={{ order: 1 }}>
+              <h6>About Us</h6>
+              <p>Careers</p>
+              <p>Global</p>
+              <p>TP Edit</p>
+              <p>Contact Us</p>
+            </Col>
+            <Col xs={{ order: 1 }}>
+              <h6>Customer Care</h6>
+              <p>Privacy Policy</p>
+              <p>Refund Policy</p>
+              <p>Shipping Policy</p>
+              <p>Terms of Service</p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
