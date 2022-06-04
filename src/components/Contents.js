589f388722cbaa4f7085ea757ffef7c82e6e3737
Muhhabbir <muhhabbir@gmail.com>
2022-06-04 18:25:10 +0700

    test

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -28,6 +28,13 @@ const Contents = () => {
                     <p class="card-text">Pulvinar leo id risus enim. Bibendum varius tincidunt pellentesque malesuada in.</p>
                 </div>
             </div>
+            <div class="card">
+                <img src="https://kucingpedia.com/wp-content/uploads/2018/05/Gambar-Kucing-Scottish-Fold-Lucu-630x380.jpg" class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                    <h5 class="card-title">Ras Kasdroen</h5>
+                    <p class="card-text">Pulvinar leo id risus enim. Bibendum varius tincidunt pellentesque malesuada in.</p>
+                </div>
+            </div>
         </div>
         </div>
     </div>
